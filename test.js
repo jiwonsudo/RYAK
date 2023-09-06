7a7df05b6b8e35fc49d19ecfe6d7361603178174
jiwonsudo <wondev112@gmail.com>
2023-09-06 20:44:24 +0900

    Fix: fix bugs

--- test.js
+++
@@ -1,9 +0,0 @@
-localStorage.setItem('manual_1', JSON.stringify({
-  name: '타이레놀 (확인용 데이터입니다.)',
-  dosage_form: '정제',
-  dosage: '500',
-  dosage_unit: 'mg',
-  image: '1_1',
-  bg_color: 'gray',
-  })
-);
